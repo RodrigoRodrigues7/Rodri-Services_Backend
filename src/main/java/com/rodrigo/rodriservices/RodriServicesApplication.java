abd16a13e591d031f5852d7cabfef5a8955d7fdd
RodrigoRodrigues7 <rodrigo7.carvalho.rodrigues@gmail.com>
2018-12-22 12:52:59 -0200

    Projeto Criado.

---
+++ src/main/java/com/rodrigo/rodriservices/RodriServicesApplication.java
@@ -0,0 +1,14 @@
+package com.rodrigo.rodriservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RodriServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RodriServicesApplication.class, args);
+	}
+
+}
+
