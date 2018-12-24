5e14d381aae6892c08b2dd6072822ecf3e46a3db
RodrigoRodrigues7 <rodrigo7.carvalho.rodrigues@gmail.com>
2018-12-24 01:04:07 -0200

    Criação e mapeamento da classe 'Produto' e criação da interface repository 'ProdutoRepository'.

--- src/main/java/com/rodrigo/rodriservices/RodriServicesApplication.java
+++ src/main/java/com/rodrigo/rodriservices/RodriServicesApplication.java
@@ -8,26 +8,42 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.rodrigo.rodriservices.Repository.CategoriaRepository;
+import com.rodrigo.rodriservices.Repository.ProdutoRepository;
 import com.rodrigo.rodriservices.domain.Categoria;
+import com.rodrigo.rodriservices.domain.Produto;
 
 @SpringBootApplication
 public class RodriServicesApplication implements CommandLineRunner {
 
 	@Autowired /* Annotation usada para instanciar o objeto automaticamente */
 	private CategoriaRepository categoriaRepository;
-	
+	@Autowired
+	private ProdutoRepository produtoRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(RodriServicesApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
-		
+
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+
 	}
 
 }
-
