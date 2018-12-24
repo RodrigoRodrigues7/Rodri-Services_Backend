d5206672096872c424df2581a4ae728f40275f8c
RodrigoRodrigues7 <rodrigo7.carvalho.rodrigues@gmail.com>
2018-12-24 01:40:20 -0200

    Ajustes no endpoint /categorias/{id} e criação de classes para tratamento de exceção caso haja pesquisa por uma categoria que não existe.

--- src/main/java/com/rodrigo/rodriservices/services/CategoriaService.java
+++ src/main/java/com/rodrigo/rodriservices/services/CategoriaService.java
@@ -7,16 +7,18 @@ import org.springframework.stereotype.Service;
 
 import com.rodrigo.rodriservices.Repository.CategoriaRepository;
 import com.rodrigo.rodriservices.domain.Categoria;
+import com.rodrigo.rodriservices.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	public Categoria buscarIdCategoria(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);		
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não Encontrado! Id: " + id + "Tipo: " + Categoria.class.getName()));
 	}
-	
+
 }
