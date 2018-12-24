d01608cfac2eec2cecf6b548be1efe687080de4c
RodrigoRodrigues7 <rodrigo7.carvalho.rodrigues@gmail.com>
2018-12-24 00:27:28 -0200

    Adicionada operação de instanciação e preenchimento automático de dois objetos do tipo 'Categoria' no banco H2.

--- src/main/java/com/rodrigo/rodriservices/RodriServicesApplication.java
+++ src/main/java/com/rodrigo/rodriservices/RodriServicesApplication.java
@@ -1,13 +1,32 @@
 package com.rodrigo.rodriservices;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class RodriServicesApplication {
+import com.rodrigo.rodriservices.Repository.CategoriaRepository;
+import com.rodrigo.rodriservices.domain.Categoria;
 
+@SpringBootApplication
+public class RodriServicesApplication implements CommandLineRunner {
+
+	@Autowired /* Annotation usada para instanciar o objeto automaticamente */
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(RodriServicesApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
