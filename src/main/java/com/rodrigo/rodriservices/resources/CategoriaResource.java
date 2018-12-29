ff4c2f22ef6834c33a6082bd63e34a5f53960a9a
RodrigoRodrigues7 <rodrigo7.carvalho.rodrigues@gmail.com>
2018-12-29 20:01:27 -0200

    Criação do método POST 'Insert' para inserir nova 'Categoria'.

--- src/main/java/com/rodrigo/rodriservices/resources/CategoriaResource.java
+++ src/main/java/com/rodrigo/rodriservices/resources/CategoriaResource.java
@@ -1,11 +1,15 @@
 package com.rodrigo.rodriservices.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.rodrigo.rodriservices.domain.Categoria;
 import com.rodrigo.rodriservices.services.CategoriaService;
@@ -22,6 +26,16 @@ public class CategoriaResource {
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		Categoria obj = service.buscarIdCategoria(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insertCategoria(@RequestBody Categoria obj){
+		
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
 	}
 	
 }
