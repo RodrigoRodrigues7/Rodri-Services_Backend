5d57089aa015e5eafec20144d5267011e117dae8
RodrigoRodrigues7 <rodrigo7.carvalho.rodrigues@gmail.com>
2018-12-29 19:11:48 -0200

    Endpoint /pedidos/{id} disponível.

---
+++ src/main/java/com/rodrigo/rodriservices/services/PedidoService.java
@@ -0,0 +1,24 @@
+package com.rodrigo.rodriservices.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rodrigo.rodriservices.Repository.PedidoRepository;
+import com.rodrigo.rodriservices.domain.Pedido;
+import com.rodrigo.rodriservices.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+
+	public Pedido buscarIdPedido(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não Encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+
+}
