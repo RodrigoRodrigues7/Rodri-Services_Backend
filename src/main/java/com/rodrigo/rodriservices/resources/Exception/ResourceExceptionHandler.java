21a6a4f49e5de27ae9cc79f34d9d74e3788c3744
RodrigoRodrigues7 <rodrigo7.carvalho.rodrigues@gmail.com>
2019-01-06 17:23:37 -0200

    Implementação do método 'DELETE' para deletar uma 'Categoria'. OBS: criada uma exceção para quando tentar deletar uma categoria que possui produtos.

--- src/main/java/com/rodrigo/rodriservices/resources/Exception/ResourceExceptionHandler.java
+++ src/main/java/com/rodrigo/rodriservices/resources/Exception/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.rodrigo.rodriservices.services.exceptions.DataIntegrityException;
 import com.rodrigo.rodriservices.services.exceptions.ObjectNotFoundException;
 
 @ControllerAdvice
@@ -19,5 +20,13 @@ public class ResourceExceptionHandler {
 				System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
 	}
+	
+	@ExceptionHandler(DataIntegrityException.class)
+	public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request) {
 
+		StandardError error = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(),
+				System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+	}
+	
 }
--- src/main/java/com/rodrigo/rodriservices/services/CategoriaService.java
+++ src/main/java/com/rodrigo/rodriservices/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.rodrigo.rodriservices.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.rodrigo.rodriservices.Repository.CategoriaRepository;
 import com.rodrigo.rodriservices.domain.Categoria;
+import com.rodrigo.rodriservices.services.exceptions.DataIntegrityException;
 import com.rodrigo.rodriservices.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -29,6 +31,15 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		findIdCategoria(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		findIdCategoria(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos.");
+		}
 	}
 	
 }
