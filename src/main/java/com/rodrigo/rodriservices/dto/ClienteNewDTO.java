2121d31195bded0719c2cbeb45640f7a74f74659
RodrigoRodrigues7 <rodrigo7.carvalho.rodrigues@gmail.com>
2019-01-28 18:59:14 -0200

    Criação de annotation customizada para validação de 'CPF' ou 'CNPJ' na inserção de 'Cliente'.

--- src/main/java/com/rodrigo/rodriservices/dto/ClienteNewDTO.java
+++ src/main/java/com/rodrigo/rodriservices/dto/ClienteNewDTO.java
@@ -2,22 +2,43 @@ package com.rodrigo.rodriservices.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.rodrigo.rodriservices.services.validation.ClienteInsert;
+
+@ClienteInsert
 public class ClienteNewDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	@NotEmpty(message="É Obrigatório o Preencimento do Nome")
+	@Length(min=5, max=120, message="O Tamanho deve ser entre 5 e 120 caracteres")
 	private String nome;
+	
+	@NotEmpty(message="É Obrigatório o Preencimento do Email")
+	@Email(message="Email Inválido")
 	private String email;
+	
+	@NotEmpty(message="É Obrigatório o Preencimento do Cpf ou Cnpj")
 	private String cpfOuCnpj;
 	private Integer tipo;
-
+	
+	@NotEmpty(message="É Obrigatório o Preencimento do Logradouro")
 	private String logradouro;
+	
+	@NotEmpty(message="É Obrigatório o Preencimento do Número")
 	private String numero;
 	private String complemento;
 	private String bairro;
+	
+	@NotEmpty(message="É Obrigatório o Preencimento do Cep")
 	private String cep;
 
 	private Integer cidadeId;
-
+	
+	@NotEmpty(message="É Obrigatório o Preencimento de ao menos 1(um) Telefones")
 	private String telefone1;
 	private String telefone2;
 	private String telefone3;
---
+++ src/main/java/com/rodrigo/rodriservices/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,41 @@
+package com.rodrigo.rodriservices.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import com.rodrigo.rodriservices.domain.enums.TipoCliente;
+import com.rodrigo.rodriservices.dto.ClienteNewDTO;
+import com.rodrigo.rodriservices.resources.Exception.FieldMessage;
+import com.rodrigo.rodriservices.services.validation.utils.BR;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Override
+	public void initialize(ClienteInsert ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+		
+		List<FieldMessage> list = new ArrayList<>();
+		
+		if(objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCodigo()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CPF Inválido"));
+		}
+		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCodigo()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CNPJ Inválido"));
+		}
+		
+		// inclua os testes aqui, inserindo erros na lista
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+			.addConstraintViolation();
+		}
+		
+		return list.isEmpty();
+	}
+}
