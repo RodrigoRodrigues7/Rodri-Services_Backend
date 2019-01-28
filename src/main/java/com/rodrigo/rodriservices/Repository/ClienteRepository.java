18323b8d6a1d828d2f568363168da5bb7b6ee8d1
RodrigoRodrigues7 <rodrigo7.carvalho.rodrigues@gmail.com>
2019-01-28 19:25:24 -0200

    Validação Customizada: email não repetido na inserção de 'Cliente'.

--- src/main/java/com/rodrigo/rodriservices/Repository/ClienteRepository.java
+++ src/main/java/com/rodrigo/rodriservices/Repository/ClienteRepository.java
@@ -2,6 +2,7 @@ package com.rodrigo.rodriservices.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.rodrigo.rodriservices.domain.Cliente;
 
@@ -9,6 +10,7 @@ import com.rodrigo.rodriservices.domain.Cliente;
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
 	
-	
+	@Transactional(readOnly=true)
+	Cliente findByEmail(String email);
 	
 }
--- src/main/java/com/rodrigo/rodriservices/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/rodrigo/rodriservices/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.rodrigo.rodriservices.Repository.ClienteRepository;
+import com.rodrigo.rodriservices.domain.Cliente;
 import com.rodrigo.rodriservices.domain.enums.TipoCliente;
 import com.rodrigo.rodriservices.dto.ClienteNewDTO;
 import com.rodrigo.rodriservices.resources.Exception.FieldMessage;
 import com.rodrigo.rodriservices.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
 	
 	@Override
 	public void initialize(ClienteInsert ann) {
@@ -22,6 +29,7 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		
 		List<FieldMessage> list = new ArrayList<>();
 		
+		// inclua os testes aqui, inserindo erros na lista
 		if(objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCodigo()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CPF Inválido"));
 		}
@@ -29,7 +37,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ Inválido"));
 		}
 		
-		// inclua os testes aqui, inserindo erros na lista
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Este Email já esta Cadastrado"));
+		}
+		
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
 			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
