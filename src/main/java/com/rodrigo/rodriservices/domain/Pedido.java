de147ffb91b195ea9fdd0919f31e1f9925aa48e2
RodrigoRodrigues7 <rodrigo7.carvalho.rodrigues@gmail.com>
2019-02-09 16:57:03 -0200

    CORREÇÃO(Inserção Pedido): Correção na classe 'Pedido' para corrigir o método insert na classe de serviço 'PedidoService'.

--- src/main/java/com/rodrigo/rodriservices/domain/Pedido.java
+++ src/main/java/com/rodrigo/rodriservices/domain/Pedido.java
@@ -16,7 +16,6 @@ import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Pedido implements Serializable {
@@ -28,12 +27,10 @@ public class Pedido implements Serializable {
 	
 	@JsonFormat(pattern="dd/MM/yyyy HH:mm")
 	private Date instante;
-	
-	@JsonIgnore
+
 	@OneToOne(cascade=CascadeType.ALL, mappedBy="pedido")
 	private Pagamento pagamento;
-	
-	@JsonIgnore
+
 	@ManyToOne
 	@JoinColumn(name="FK_Id_Cliente")
 	private Cliente cliente;
