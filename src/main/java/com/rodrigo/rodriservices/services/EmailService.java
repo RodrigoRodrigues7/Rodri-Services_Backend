010fb507c045011908320732b0db407cf82922f4
RodrigoRodrigues7 <rodrigo7.carvalho.rodrigues@gmail.com>
2019-02-12 02:29:02 -0200

    Implementação de envio de email com html.

--- src/main/java/com/rodrigo/rodriservices/services/EmailService.java
+++ src/main/java/com/rodrigo/rodriservices/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.rodrigo.rodriservices.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.rodrigo.rodriservices.domain.Pedido;
@@ -7,7 +9,9 @@ import com.rodrigo.rodriservices.domain.Pedido;
 public interface EmailService {
 	
 	void sendOrderConfirmationEmail(Pedido obj);
-	
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	void sendHtmlEmail(MimeMessage msg);
 	
 }
--- src/main/java/com/rodrigo/rodriservices/services/PedidoService.java
+++ src/main/java/com/rodrigo/rodriservices/services/PedidoService.java
@@ -66,7 +66,7 @@ public class PedidoService {
 		
 		itemPedidoRepo.saveAll(obj.getItens());
 		//System.out.println(obj);
-		emailService.sendOrderConfirmationEmail(obj);
+		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
 	}
 	
