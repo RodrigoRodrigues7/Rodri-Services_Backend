b7f696a54d4cfecc0d67a48e3ed4bf3ffc10a0e1
RodrigoRodrigues7 <rodrigo7.carvalho.rodrigues@gmail.com>
2019-02-21 17:35:56 -0300

    Implementado o Refresh Token.

---
+++ src/main/java/com/rodrigo/rodriservices/resources/AuthResource.java
@@ -0,0 +1,31 @@
+package com.rodrigo.rodriservices.resources;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rodrigo.rodriservices.security.JWTUtil;
+import com.rodrigo.rodriservices.security.UserSpringSecurity;
+import com.rodrigo.rodriservices.services.UserService;
+
+@RestController
+@RequestMapping(value ="/auth")
+public class AuthResource {
+
+	@Autowired
+	private JWTUtil jwtUtil;
+	
+	@RequestMapping(value="/refresh_token", method=RequestMethod.POST)
+	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+		
+		UserSpringSecurity user = UserService.authenticated();
+		String token = jwtUtil.generateToken(user.getUsername());
+		response.addHeader("Authorization", "Bearer " + token);
+		return ResponseEntity.noContent().build();
+	}
+
+}
