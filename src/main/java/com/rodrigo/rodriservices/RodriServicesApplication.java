a184166b0656c96633b9fb3491bcdf1f82b018c0
RodrigoRodrigues7 <rodrigo7.carvalho.rodrigues@gmail.com>
2019-02-25 17:34:40 -0300

    Salvando primeira imagem no 'Amazon S3'.

--- src/main/java/com/rodrigo/rodriservices/RodriServicesApplication.java
+++ src/main/java/com/rodrigo/rodriservices/RodriServicesApplication.java
@@ -1,19 +1,25 @@
 package com.rodrigo.rodriservices;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.rodrigo.rodriservices.services.S3Service;
+
 @SpringBootApplication
 public class RodriServicesApplication implements CommandLineRunner {
 
+	@Autowired
+	private S3Service s3Service;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(RodriServicesApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {	
-
+		s3Service.uploadFile("C:\\Users\\rodri_000\\Pictures\\chun-lo-kylo-red(mod).jpg");
 	}
 
 }
